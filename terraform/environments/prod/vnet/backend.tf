022ba5e0e4f87486af65667b08dd018c7c9f3d56
visala123 <visala.k@algorims.com>
2025-08-27 16:18:28 +0800

    initial commit

--- terraform/environments/prod/vnet/backend.tf
+++ terraform/environments/prod/vnet/backend.tf
@@ -1,8 +1,11 @@
 terraform {
   backend "azurerm" {
-   resource_group_name  = "dev-revinci"
-    storage_account_name = "devrevincicicd"
-    container_name       = "dev-revinci"
+  #  resource_group_name  = "dev-revinci"
+  #   storage_account_name = "devrevincicicd"
+  #   container_name       = "dev-revinci"
+   resource_group_name  = "demo-rg"
+    storage_account_name = "terraformbackendalgorims"
+    container_name       = "tfstate-aks"
     key                  = "prod.vnet.tfstate"
   }
 }
